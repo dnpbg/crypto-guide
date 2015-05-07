df26d43731cd778d9e6b89856427f11df3c2b0fc
Rumen Petrov <rumenpetrow@gmail.com>
2015-05-07 22:08:41 +0300

    style inner page

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@
 
 		// fullscreener plugin
 		$('.bg img').fullscreener()
+
+		// tabs
+		var $tabs = $('.tab'),
+			$tabsNav = $('.tabs-nav');
+
+		$tabs.not(':first-child').hide();
+		$tabsNav.find('li:first-child').addClass('current');
+
+		$tabsNav.on('click', 'a', function(event){
+			var $this = $(this),
+				$parent = $this.parent(),
+				href = $this.attr('href');
+
+			if (!$parent.hasClass('current')) {
+				$parent.addClass('current');
+				$parent.siblings().removeClass('current');
+				$tabs.filter(href).show().siblings().hide();
+			}
+
+			event.preventDefault();
+
+		});
 	});
 })(jQuery);
